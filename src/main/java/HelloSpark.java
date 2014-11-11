3b84a5bd03b0dec0e2133f56946b3ff06b8cd17b
Henry Stanley <henry@henrystanley.com>
2014-11-11 22:40:18 +0000

    updated HelloSpark class - now we're online

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -1,8 +1,16 @@
 /**
  * Created by henryaj on 11/11/14.
  */
+import static spark.Spark.*;
+import spark.*;
+
 public class HelloSpark {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "hello!";
+            }
+        });
     }
 }
